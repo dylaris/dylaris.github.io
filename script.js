31d4519f90f431766b869df4653f161c5f02e5a1
Dylaris <3445818679@qq.com>
2026-01-25 16:01:30 +0800

    support display blog in page

--- script.js
+++ script.js
@@ -5,15 +5,46 @@ function openPage(pageName, element) {
         tabcontent[i].style.display = "none";
     }
 
+    // remove class 'active'
     let tablink = document.getElementsByClassName("tab-link");
     for (let i = 0; i < tablink.length; i++) {
-        tablink[i].style.backgroundColor = "";
+        tablink[i].classList.remove("active");
     }
 
     // display current page
     document.getElementById(pageName).style.display = "block";
-    const root = document.documentElement;
-    const color = getComputedStyle(root).getPropertyValue(`--bg-body-${pageName}`).trim();
-    element.style.backgroundColor = color;
-    document.body.style.backgroundColor = color;
+    element.classList.add("active");
+
+    if (pageName == "blog") {
+        document.getElementById("blog-list").style.display = "block";
+        document.getElementById("blog-detail").style.display = "none";
+    }
+}
+
+function loadBlogContent(blogId) {
+    const blogPathMap = {
+        0: "posts/template.html",
+        1: 'posts/The "One-and-Done" Memory Allocator: A Primer on Arena Allocation.html',
+    };
+    const blogPath = blogPathMap[blogId];
+    if (!blogPath) return;
+
+    // hide the list, display the detail container
+    document.getElementById("blog-list").style.display = "none";
+    const blogDetail = document.getElementById("blog-detail");
+    blogDetail.style.display = "block";
+    blogDetail.innerHTML = '<div style="color: var(--color-text-2);">Loading...</div>';
+
+    // display blog content
+    fetch(blogPath)
+        .then(response => {
+            if (!response.ok) throw new Error("Failed to laod blog");
+            return response.text();
+        })
+        .then(html => {
+            blogDetail.innerHTML = `<div>${html}</div>`;
+        })
+        .catch(err => {
+            blogDetail.innerHTML = `<div style="color: #ff4444;">${err.message}</div>`;
+        });
 }
