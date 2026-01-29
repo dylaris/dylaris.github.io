3360e5d69e2d7008e9e413980e2ddb086c25e51c
Dylaris <3445818679@qq.com>
2026-01-29 17:26:50 +0800

    add toc and title back to toc

--- script.js
+++ script.js
@@ -21,11 +21,23 @@ function openPage(pageName, element) {
     }
 }
 
+let blogPathMap = {};
+
+function initBlogIds() {
+    blogPathMap = {};
+    const blogUl = document.getElementById("blog-ul");
+    const blogItems = blogUl.getElementsByTagName("li");
+    for (let i = 0; i < blogItems.length; i++) {
+        const blogLink = blogItems[i].getElementsByTagName("a")[0];
+        blogLink.setAttribute("data-blog-id", i);
+        const blogTitle = blogLink.innerText.trim();
+        const blogFileName = blogTitle;
+        blogPathMap[i] = `posts/${blogFileName}.html`;
+    }
+    console.log(blogPathMap);
+}
+
 function loadBlogContent(blogId) {
-    const blogPathMap = {
-        0: "posts/template.html",
-        1: 'posts/The "One-and-Done" Memory Allocator: A Primer on Arena Allocation.html',
-    };
     const blogPath = blogPathMap[blogId];
     if (!blogPath) return;
 
@@ -33,7 +45,7 @@ function loadBlogContent(blogId) {
     document.getElementById("blog-list").style.display = "none";
     const blogDetail = document.getElementById("blog-detail");
     blogDetail.style.display = "block";
-    blogDetail.innerHTML = '<div style="color: var(--color-text-2);">Loading...</div>';
+    blogDetail.innerHTML = '<div style="color: var(--fg);">Loading...</div>';
 
     // display blog content
     fetch(blogPath)
@@ -43,8 +55,65 @@ function loadBlogContent(blogId) {
         })
         .then(html => {
             blogDetail.innerHTML = `<div>${html}</div>`;
+            generateBlogToc();
         })
         .catch(err => {
             blogDetail.innerHTML = `<div style="color: #ff4444;">${err.message}</div>`;
         });
+}
+
+function generateBlogToc() {
+    const blogDetail = document.getElementById("blog-detail");
+    const titles = blogDetail.querySelectorAll("h1, h2, h3, h4");
+
+    // create toc and insert it to beginning
+    const tocContainer = document.createElement("div");
+    tocContainer.className = "toc";
+    tocContainer.id = "blog-toc"
+    const tocUl = document.createElement("ul");
+    tocContainer.appendChild(tocUl);
+
+    const header = blogDetail.querySelector("h1");
+    const headerParent = header.parentNode;
+    headerParent.insertBefore(tocContainer, header.nextSibling);
+
+    // add id for each title and toc navigation
+    for (let i = 0; i < titles.length; i++) {
+        const title = titles[i];
+        const tagName = title.tagName.toLowerCase();
+        const id = `${i}-${tagName}`;
+        title.id = id;
+
+        // click title and goto toc
+        title.style.cursor = "pointer";
+        title.addEventListener("click", () => {
+            const toc = document.getElementById("blog-toc");
+            if (toc) {
+                toc.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+            }
+        });
+
+        const tocLi = document.createElement("li");
+        const tocLink = document.createElement("a");
+
+        tocLink.textContent = title.textContent.trim();
+        // tocLink.href = `#${id}`;
+        tocLink.style.cursor = "pointer";
+        tocLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetTitle = document.getElementById(id);
+            if (targetTitle) {
+                targetTitle.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+            }
+        });
+
+        tocLi.appendChild(tocLink);
+        tocUl.appendChild(tocLi);
+    }
 }
